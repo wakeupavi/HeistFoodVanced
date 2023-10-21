25b99c150c2634223827957998c3060daa3d76ed
avigupta250 <avinashkumarnav52@gmail.com>
2023-10-21 18:39:53 +0530

    modified

--- src/utils/useGetAllrestau.js
+++ src/utils/useGetAllrestau.js
@@ -20,7 +20,8 @@ export const useGetAllrestau=(RES_LIST_URL)=>{
       const json = await data.json();
   
   
-       // was showing an error of data fatching because sometime data coming from cards[1] sometime cards[2] and different on other times so me make a function and check which value of i gives data in cards[i];
+       // was showing an error of data fatching because sometime data coming from cards[1] sometime cards[2]
+        // and different on other times so me make a function and check which value of i gives data in cards[i];
        let i = 0;
        async function checkJsonData(jsonData) {
         
